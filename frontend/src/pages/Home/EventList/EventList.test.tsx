974f888cc9a4b24dfa5d0990e7dccbfbde4562e6
dohaicuong <beatyshot@gmail.com>
2020-08-08 21:38:31 +1000

    add event list test

---
+++ frontend/src/pages/Home/EventList/EventList.test.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { render, cleanup, waitFor } from '@testing-library/react'
+import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils'
+import { RelayEnvironmentProvider } from 'react-relay/hooks'
+
+import EventList from '.'
+
+afterEach(cleanup)
+
+describe('<EventList />', () => {
+  it('should reject query and catched by error boundary', async () => {
+    const environment = createMockEnvironment()
+    const { getByText } = render(
+      <RelayEnvironmentProvider environment={environment}>
+        <EventList />
+      </RelayEnvironmentProvider>
+    )
+
+    environment.mock.rejectMostRecentOperation(operation => new Error(`Some error`))
+    await waitFor(() => expect(getByText('Something went wrong:')).toBeTruthy())
+  })
+  it('should render loading then render EventList', async () => {
+    const environment = createMockEnvironment()
+    const { getByText, findByText, container } = render(
+      <RelayEnvironmentProvider environment={environment}>
+        <EventList />
+      </RelayEnvironmentProvider>
+    )
+
+    expect(getByText('Loading events...')).toBeTruthy()
+
+    environment.mock.resolveMostRecentOperation(operation => {
+      return MockPayloadGenerator.generate(operation, {
+        PageInfo() {
+          return {
+            "hasNextPage": true,
+            "hasPreviousPage": false,
+            "startCursor": "YXJyYXljb25uZWN0aW9uOjA=",
+            "endCursor": "YXJyYXljb25uZWN0aW9uOjE="
+          }
+        },
+        WorldEdge() {
+          return [
+            {
+              "cursor": "YXJyYXljb25uZWN0aW9uOjA=",
+              "node": {
+                "id": "RXZlbnQ6Y2tkbGRmZnc2MDAxOHJzeGFkN3UwYTg3Yg==",
+                "title": "Place 1",
+                "time": "2018-07-22T02:30:00.000Z",
+                "image": "http://example.com/image.png",
+                "location": {
+                  "city": "Brisbane",
+                  "state": "Queensland",
+                  "country": "Australia"
+                },
+                "availableSeats": []
+              }
+            },
+            {
+              "cursor": "YXJyYXljb25uZWN0aW9uOjE=",
+              "node": {
+                "id": "RXZlbnQ6Y2tkbGRmZzFwMDAyNHJzeGE2N3pjcWs3eg==",
+                "title": "Place 2",
+                "time": "2018-07-24T02:30:00.000Z",
+                "image": "http://example.com/image.png",
+                "location": {
+                  "city": "Cairns",
+                  "state": "Queensland",
+                  "country": "Australia"
+                },
+                "availableSeats": [
+                  {
+                    "id": "U2VhdDpCMjk="
+                  },
+                  {
+                    "id": "U2VhdDpXMjU="
+                  }
+                ]
+              }
+            }
+          ]
+        },
+      })
+    })
+
+    await findByText(/seats: /)
+    expect(container).toMatchSnapshot()
+  })
+})

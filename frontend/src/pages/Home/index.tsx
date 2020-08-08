f0b8b815619ec8e63b7cae04a1cd6a8bc8f71cd3
dohaicuong <beatyshot@gmail.com>
2020-08-08 20:29:45 +1000

    render event list

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { Container, Typography, Paper } from '@material-ui/core'
+import EventList from './EventList'
 
 const Home: React.FC = () => {
-  return <>Home</>
+  return (
+    <>
+      Home
+      <Container maxWidth='md'>
+        <Typography variant='h4' gutterBottom>
+          Event list
+        </Typography>
+        <Paper>
+          <EventList />
+        </Paper>
+      </Container>
+    </>
+  )
 }
 export default Home
